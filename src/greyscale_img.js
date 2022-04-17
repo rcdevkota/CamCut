6a1009f35c8fc09bc97f2b278fc7e2d0c02ac744
Seron95 <Jeheit95@web.de>
2022-04-17 22:17:39 +0200

    single image grayscale

--- src/greyscale_img.js
+++ src/greyscale_img.js
@@ -1,9 +1,35 @@
-const canvas = document.getElementById("c1");
-const ctx = canvas.getContext("2d");
-const image = document.getElementById("sample_image");
+var ctx = c.getContext("2d"), img = new Image;
+img.onload = setup;  img.crossOrigin = "";
+img.src = "//i.imgur.com/OrYVGI8.jpg";
 
-image.addEventListener('load', e => {
-    // Draw image with filter
-    ctx.filter = "greyscale(100%)";
-    ctx.drawImage(image, 400, 0, -image.width * .6, image.height * .6);
-});
+function setup() {
+  c.width = this.naturalWidth;  c.height = this.naturalHeight;
+  ctx.drawImage(this, 0, 0);    btn.disabled = false
+}
+
+// Main code for demo
+btn.onclick = function() {
+  
+  var idataSrc = ctx.getImageData(0, 0, c.width, c.height), // original
+      idataTrg = ctx.createImageData(c.width, c.height),    // empty data
+      dataSrc = idataSrc.data,                              // reference the data itself
+      dataTrg = idataTrg.data,
+      len = dataSrc.length, i = 0, luma;
+  
+  // convert by iterating over each pixel each representing RGBA
+  for(; i < len; i += 4) {
+    // calculate luma, here using Rec 709
+    luma = dataSrc[i] * 0.2126 + dataSrc[i+1] * 0.7152 + dataSrc[i+2] * 0.0722;
+
+    // update target's RGB using the same luma value for all channels
+    dataTrg[i] = dataTrg[i+1] = dataTrg[i+2] = luma;
+    dataTrg[i+3] = dataSrc[i+3];                            // copy alpha
+  }
+  
+  // put back luma data so we can save it as image
+  ctx.putImageData(idataTrg, 0, 0);
+  demo.src = c.toDataURL();                                 // set demo result's src url
+  
+  // restore backup data
+  ctx.putImageData(idataSrc, 0, 0);
+};
