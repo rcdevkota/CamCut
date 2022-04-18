7ca3241ad028b53c14af9d53b283eb8dd0f19033
Seron95 <Jeheit95@web.de>
2022-04-18 15:52:18 +0200

    Greyscaling script now works for videos

--- src/greyscale.js
+++ src/greyscale.js
@@ -1,44 +1,42 @@
 
-let processor = {
-    timerCallback: function () {
+var processor = {
+    timerCallback: function() {
         if (this.video.paused || this.video.ended) {
             return;
         }
         this.computeFrame();
-        let self = this;
+        var self = this;
         setTimeout(function () {
             self.timerCallback();
         }, 16); // roughly 60 frames per second
     },
 
-    doLoad: function () {
+    doLoad: function() {
         this.video = document.getElementById("sample_video");
         this.c1 = document.getElementById("c1");
         this.ctx1 = this.c1.getContext("2d");
-        let self = this;
+        var self = this;
 
-        this.video.addEventListener("play", function () {
+        this.video.addEventListener("play", function() {
             self.width = self.video.width;
             self.height = self.video.height;
             self.timerCallback();
         }, false);
     },
 
-    computeFrame: function () {
+    computeFrame: function() {
         this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
-        let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
-        let l = frame.data.length / 4;
+        var frame = this.ctx1.getImageData(0, 0, this.width, this.height);
+        var l = frame.data.length / 4;
 
-        for (let i = 0; i < l; i++) {
-            let grey = (frame.data[i * 4 + 0] + frame.data[i * 4 + 1] + frame.data[i * 4 + 2]) / 3;
+        for (var i = 0; i < l; i++) {
+            var grey = (frame.data[i * 4] + frame.data[i * 4 + 1] + frame.data[i * 4 + 2]) / 3;
 
-            frame.data[i * 4 + 0] = grey;
+            frame.data[i * 4] = grey;
             frame.data[i * 4 + 1] = grey;
             frame.data[i * 4 + 2] = grey;
         }
         this.ctx1.putImageData(frame, 0, 0);
-
-        return;
     }
 };
 
