1e512179033deb2fbdcbde1e479ef874ca0a89ad
PeerOleA <peerolea@gmail.com>
2022-05-11 08:41:34 +0200

    kamera fix

--- backend/liveView.js
+++ backend/liveView.js
@@ -1,41 +1,44 @@
 const checkboxCam = document.getElementById("checkbox📸");
 const checkboxScreen = document.getElementById("checkbox🖥");
-const  videoElemWebCam = document.getElementById(" videoElemWebCam");
+const  videoElemWebCam = document.getElementById("videoElemWebCam");
 const videoElemScreen = document.getElementById("videoElementScreen");
 //json parameters for the recording of the screen 
-  var displayMediaOptions = {
-      video: {
-        cursor: "always",
-      },
-      audio: false,
-    };
-
+var displayMediaOptions = {
+	video: {
+		cursor: "always",
+	},
+	audio: false,
+};
 checkboxCam.addEventListener("click", () => {
-if (document.getElementById("checkbox📸").checked) {
-        console.log(" gecheckt");
-    showWebcam();
-    }
-});
-checkboxScreen.addEventListener("click", () => {
-if (document.getElementById("checkbox📸").checked) {
-        console.log(" gecheckt");
-    showScreen();
-    }
+	if (checkboxCam.checked) {
+		showWebcam();
+	}else{
+		let tracks = videoElemWebCam.srcObject.getTracks();
+		tracks.forEach(track => track.stop());
+		videoElemWebCam.srcObject = null;
+	}
 });
 
- async function showWebcam() {
-      try {
-        videoElemWebCam.srcObject = await navigator.mediaDevices.getUserMedia(displayMediaOptions);
-      } catch (err) {
-        console.error("Error: " + err);
-      }
- }
-     async function showScreen() {
-      try {
-        videoElemScreen.srcObject = await navigator.mediaDevices.getDisplayMedia(
-          displayMediaOptions
-        );
-      } catch (err) {
-        console.error("Error: " + err);
-      }
-    }
+checkboxScreen.addEventListener("click", () => {
+	if (checkboxScreen.checked) {
+		showScreen();
+	}else{
+		let tracks = videoElemScreen.srcObject.getTracks();
+		tracks.forEach(track => track.stop());
+		videoElemScreen.srcObject = null;
+	}
+});
+async function showWebcam() {
+	try {
+		videoElemWebCam.srcObject = await navigator.mediaDevices.getUserMedia(displayMediaOptions);
+	} catch (err) {
+		checkboxCam.checked = false;
+	}
+}
+async function showScreen() {
+	try {
+		videoElemScreen.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+	} catch (err) {
+		checkboxScreen.checked = false;
+	}
+}
