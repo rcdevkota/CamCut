65e1f4e4f9986d04b6b562afe1a107ec87b73a86
PeerOleA <peerolea@gmail.com>
2022-06-06 07:33:54 +0200

    dark mode save

--- backend/main.js
+++ backend/main.js
@@ -1,4 +1,15 @@
-const checkboxDark = document.getElementById("checkboxDark");
-checkboxDark.addEventListener("click",()=>{
-    document.body.classList.toggle("dark_mode")
+document.getElementById("checkboxDark").addEventListener("click",()=>{
+    document.body.classList.toggle("dark_mode");
+
+    if(document.cookie=="dark_mode"){
+        document.cookie  = null;
+
+    }else{
+        document.cookie = "dark_mode";
+    }
 })
+
+if(document.cookie=="dark_mode"){
+    document.body.classList.toggle("dark_mode");
+    document.getElementById("checkboxDark").checked=true;
+}
